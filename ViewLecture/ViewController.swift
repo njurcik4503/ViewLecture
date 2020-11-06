ed53cd1db59d4f3c53c5525fc42c00eab1f31b05
natalie <njurcik4503@stu.d214.org>
2020-11-06 15:19:57 -0600

    Added functionality to add to the number in the textfield

--- ViewLecture/ViewController.swift
+++ ViewLecture/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var startNumberTextField: UITextField!
+    
+    var counter: Int = Int()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        let data = startNumberTextField.text!
+        counter = Int(data)!
+    }
+    
+    @IBAction func whenCoolButtonPressed(_ sender: Any) {
+        counter += 1
+        numberLabel.text = "\(counter)"
+    }
+    
+    
 }
 
